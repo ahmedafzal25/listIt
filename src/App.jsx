b11e679118126bb9af2fa2f2408a953ec07f9470
ahmedafzal25 <muhammadahmedafzal25@gmail.com>
2026-04-08 06:26:19 +0500

    Initial listIt setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to listIt</h1>
+      <p>The industrial-grade property platform.</p>
+    </div>
+  );
+}
+
+export default App;
